2c82271b290d5fe7598e8fe37de0628d2009645b
Nisen <imnisen@gmail.com>
2018-05-18 23:00:06 +0800

    v4-** ,version1.2

--- v4-tns-restful-json-api/handlers.go
+++ v4-tns-restful-json-api/handlers.go
@@ -15,9 +15,14 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 	todos := Todos{
 		Todo{Name: "Write presentation"},
 		Todo{Name: "Host meetup"},
-
 	}
-	json.NewEncoder(w).Encode(todos)
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
 
 }
 
